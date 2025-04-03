3123071c0ff277a7722caba99bec6e39bb55631e
TheGamerLORE <batkingluis44@gmail.com>
2025-04-03 15:52:46 -0400

    submited all files

---
+++ user_interface_1/Triggers.cs
@@ -0,0 +1,9 @@
+/*using UnityEngine;
+
+public class Triggers
+// This function is called when this GameObject enters a trigger collider
+void OnTriggerEnter(collider2D other) { 
+    //
+    
+}
+*/
